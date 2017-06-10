a6f1bd14f72615b33399248fc162bba7b68add63
Luke Chambers <lukechambers91@gmail.com>
2017-06-10 16:32:46 -0400

    finished basic template

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function() {
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAPp8Pd0Z9IqinCTYgGpEFj1EBw3ev90WU",
+    authDomain: "rpsgamehw.firebaseapp.com",
+    databaseURL: "https://rpsgamehw.firebaseio.com",
+    projectId: "rpsgamehw",
+    storageBucket: "rpsgamehw.appspot.com",
+    messagingSenderId: "399888694448"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+  
+
+});
